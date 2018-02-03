542cf581bb6e5c3d253dd3a7027a7218526df9c5
jethrogillgren <jethrogillgren@gmail.com>
2018-02-03 17:59:26 +0000

    Updated network messages

--- KoalaCube/KoalaCube.ino
+++ KoalaCube/KoalaCube.ino
@@ -2,27 +2,13 @@
 
 /*
  * --------------------------------------------------------------------------------------------------------------------
- * Example sketch/program showing how to read data from a PICC to serial.
+ * Controls local LED from server messages, and lets server know if it has found a Tag.
  * --------------------------------------------------------------------------------------------------------------------
- * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
  * 
- * Example sketch/program showing how to read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
- * Reader on the Arduino SPI interface.
+ * This is the moving KoalaCube object code.
+ * Mesh Network of XBees.  Messages are cubeId:char  with cubeId as the destination or the sender.
  * 
- * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
- * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
- * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
- * will show the ID/UID, type and any data blocks it can read. Note: you may see "Timeout in communication" messages
- * when removing the PICC from reading distance too early.
- * 
- * If your reader supports it, this sketch/program will read all the PICCs presented (that is: multiple tag reading).
- * So if you stack two or more PICCs on top of each other and present them to the reader, it will first output all
- * details of the first and then the next PICC. Note that this may take some time as all data blocks are dumped, so
- * keep the PICCs at reading distance until complete.
- * 
- * @license Released into the public domain.
- * 
- * Typical pin layout used:
+ * Pin layout used:
  * -----------------------------------------------------------------------------------------
  *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
  *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
@@ -33,10 +19,14 @@
  * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
  * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
  * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ * 
+ * TODO - KoalaCube full pins
  */
 
 #include <SPI.h>
 #include <MFRC522.h>
+
+int cubeId = 0; //Manually set for each different cube build.
 
 constexpr uint8_t RST_PIN = 9;          // Configurable, see typical pin layout above
 constexpr uint8_t SS_PIN = 10;         // Configurable, see typical pin layout above
@@ -47,11 +37,14 @@ float sendHz = 1;
 #define GREEN_PIN 5
 #define RED_PIN 6
 
-#define MSG_SET_COLOUR_BLUE 'B'
-#define MSG_SET_COLOUR_RED 'R'
-#define MSG_SET_COLOUR_GREEN 'G'
-#define MSG_SET_COLOUR_WHITE 'W'
+String msgSeperator = ':'               //Message content is after this char.  Dest is before it.
+#define MSG_SET_COLOUR_BLUE   'B'
+#define MSG_SET_COLOUR_RED    'R'
+#define MSG_SET_COLOUR_GREEN  'G'
+#define MSG_SET_COLOUR_WHITE  'W'
 #define MSG_SET_COLOUR_YELLOW 'Y'
+#define MSG_SET_COLOUR_NONE   'N'
+
 
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
@@ -107,7 +100,7 @@ void loop() {
       send_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
       Serial.println();
       
-      delay(1000/sendHz);                       // wait for a second
+      delay(1000/sendHz);                       // wait for a second (makes L flash off)
     }
 
   // Dump debug info about the card; PICC_HaltA() is automatically called
@@ -122,24 +115,28 @@ void parseSerialInput(char c)
   
   switch (c)
   {
-  case MSG_SET_COLOUR_RED: // Send a frame out next. 
+  case cubeId+msgSeperator+MSG_SET_COLOUR_RED: 
     SetColourRed();
     break;
 
-  case MSG_SET_COLOUR_BLUE: // Send a frame out next. 
+  case cubeId+msgSeperator+MSG_SET_COLOUR_BLUE: 
     SetColourBlue();
     break;
 
-  case MSG_SET_COLOUR_WHITE: // Send a frame out next. 
+  case cubeId+msgSeperator+MSG_SET_COLOUR_WHITE: 
     SetColourWhite();
     break;
 
-  case MSG_SET_COLOUR_GREEN: // Send a frame out next. 
+  case cubeId+msgSeperator+MSG_SET_COLOUR_GREEN: 
     SetColourGreen();
     break;
 
-  case MSG_SET_COLOUR_YELLOW: // Send a frame out next. 
+  case cubeId+msgSeperator+MSG_SET_COLOUR_YELLOW: 
     SetColourYellow();
+    break;
+
+  case cubeId+msgSeperator+MSG_SET_COLOUR_NONE:
+    SetColourNone();
     break;
 
   default: // If an invalid character, do nothing
@@ -176,6 +173,9 @@ void SetColourWhite() {
 }
 void SetColourBlue() {
   SetColour(0,0,255);
+}
+void SetColourNone() {
+  SetColour(0,0,0);
 }
 void SetColour(int red, int green, int blue)
 {
